541a0eee7f5c15a954fb6e143ba3350ff2da8eb5
haruki26 <h826.a519@gmail.com>
2026-01-24 06:16:00 +0000

    chore: リンター系の設定ファイルをignore

--- app/frontend/eslint.config.js
+++ app/frontend/eslint.config.js
@@ -2,4 +2,16 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    ignores: ['./eslint.config.js', './prettier.config.js'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+]
