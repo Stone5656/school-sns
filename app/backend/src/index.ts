f65fb885ec1e5ff24533d320388517cd91757d52
jo <jo.yasuda.dev@gmail.com>
2026-01-28 12:33:15 +0000

    fix: serveをtest時に使用しないように変更

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -30,12 +30,14 @@ export const app = new Hono()
 
 export type AppType = typeof app
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server running on http://localhost:${info.port.toString()}`)
-  },
-)
+if (process.env.NODE_ENV !== 'test') {
+  serve(
+    {
+      fetch: app.fetch,
+      port: 3000,
+    },
+    (info) => {
+      console.log(`Server running on http://localhost:${info.port.toString()}`)
+    },
+  )
+}
