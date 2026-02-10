8ad25e700d2a80fbd1014d95366d1baa039e1fca
安田丈 <131738157+Stone5656@users.noreply.github.com>
2026-02-10 12:49:21 +0900

    Update index.ts

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,35 +1,36 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { summarizePost } from './lib/langchain/index.js'
+// import { Hono } from 'hono'
+// import { cors } from 'hono/cors'
+import { app } from './routes/index.js'
+// import { summarizePost } from './lib/langchain/index.js'
 
-export const app = new Hono()
-  .use(
-    '*',
-    cors({
-      origin: 'https://school-sns.jo-yasuda-dev.workers.dev',
-      allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
-      allowHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    }),
-  )
-  .get('/', (c) => {
-    return c.text('Hello Hono!')
-  })
-  .get('/demo', (c) => {
-    return c.json({ message: 'This is a demo endpoint.' })
-  })
-  .get('/summarize/demo', async (c) => {
-    try {
-      const response = await summarizePost(
-        `LangChainは、言語モデルを活用したアプリケーションを構築するためのフレームワークです。PythonやJavaScriptで利用でき、チャットボット、ドキュメント解析、データ生成など多様な用途に対応しています。モジュール式の設計により、カスタマイズや拡張が容易であり、オープンソースコミュニティによって積極的に開発されています。LangChainを使用することで、開発者は高度な言語処理機能を迅速に実装でき、AI駆動型アプリケーションの開発が加速します。`,
-      )
-      return c.json({ summary: response.content, metadata: response })
-    } catch (err) {
-      console.error(err)
-      return c.json({ error: 'Internal Server Error' }, 500)
-    }
-  })
+// export const app = new Hono()
+//   .use(
+//     '*',
+//     cors({
+//       origin: 'https://school-sns.jo-yasuda-dev.workers.dev',
+//       allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+//       allowHeaders: ['Content-Type', 'Authorization'],
+//       credentials: true,
+//     }),
+//   )
+//   .get('/', (c) => {
+//     return c.text('Hello Hono!')
+//   })
+//   .get('/demo', (c) => {
+//     return c.json({ message: 'This is a demo endpoint.' })
+//   })
+//   .get('/summarize/demo', async (c) => {
+//     try {
+//       const response = await summarizePost(
+//         `LangChainは、言語モデルを活用したアプリケーションを構築するためのフレームワークです。PythonやJavaScriptで利用でき、チャットボット、ドキュメント解析、データ生成など多様な用途に対応しています。モジュール式の設計により、カスタマイズや拡張が容易であり、オープンソースコミュニティによって積極的に開発されています。LangChainを使用することで、開発者は高度な言語処理機能を迅速に実装でき、AI駆動型アプリケーションの開発が加速します。`,
+//       )
+//       return c.json({ summary: response.content, metadata: response })
+//     } catch (err) {
+//       console.error(err)
+//       return c.json({ error: 'Internal Server Error' }, 500)
+//     }
+//   })
 
 export type AppType = typeof app
 
